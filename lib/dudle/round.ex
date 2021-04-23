0fe6310a090caed9243e72f96abc8f22a9bd5279
Max Tyler <maxastyler@gmail.com>
2021-04-23 21:02:26 +0100

    Make some functions for the round

--- lib/dudle/round.ex
+++ lib/dudle/round.ex
@@ -14,7 +14,8 @@ defmodule Dudle.Round do
   """
   @spec pick_prompts([player()], [String.t()]) :: prompt_map()
   defp pick_prompts(players, prompts) do
-    for {player, prompt} <- Enum.zip(players, Enum.take_random(prompts, length(players))), into: %{} do
+    for {player, prompt} <- Enum.zip(players, Enum.take_random(prompts, length(players))),
+        into: %{} do
       {player, {:text, "initial prompt", prompt}}
     end
   end
@@ -24,11 +25,77 @@ defmodule Dudle.Round do
       {:error, "Less prompts than there are players"}
     else
       prompts = pick_prompts(players, possible_prompts)
+
       round = %{
-        submissions: (for {p, s} <- prompts, into: %{}, do: {p, [s]}),
-        prompts: prompts,
+        submissions: for({p, s} <- prompts, into: %{}, do: {p, [s]}),
+        prompts: prompts
       }
+
       {:ok, round}
+    end
+  end
+
+  defp mod(x, n) do
+    x = rem(x, n)
+
+    if x < 0 do
+      x + n
+    else
+      x
+    end
+  end
+
+  defp rotate(rs, 0), do: rs
+  defp rotate([r | rs], n), do: rotate(List.insert_at(rs, -1, r), n - 1)
+  defp rotate(r, n) when length(r) < 0 or length(r) >= n, do: rotate(r, mod(length(r), n))
+
+  @doc """
+  Convert the round from a playing state into a reviewing state
+  """
+  def convert_to_reviewing_state(%{submissions: submissions} = round, players) do
+    reversed = for {p, s} <- submissions, into: %{}, do: {p, Enum.reverse(s)}
+
+    review =
+      for [_, {_, player, _} | _] = v <-
+            Enum.map(players, &reversed[&1])
+            |> Enum.zip()
+            |> Enum.zip([0 | Enum.to_list(0..length(players))])
+            |> Enum.map(fn {l, i} -> rotate(Tuple.to_list(l), i) end)
+            |> Enum.zip()
+            |> Enum.map(&Tuple.to_list(&1)),
+          into: %{} do
+        {player, v}
+      end
+
+    round
+    |> Map.drop([:submissions])
+    |> Map.put(:prompts, review)
+    |> Map.put(:review_state, {List.first(players), 0})
+    |> Map.put(:reviewers_left, Enum.drop(players, 1))
+  end
+
+  @doc """
+  Get the current prompt to review from the round
+  """
+  def get_review_prompt(%{review_state: {player, element}, prompts: prompts} = _round) do
+    prompts[player] |> Enum.at(element)
+  end
+
+  @doc """
+  Get the next review state of the round, returning :finished if done
+  """
+  def get_next_review_state(
+        %{review_state: {player, element}, prompts: prompts, reviewers_left: reviewers} = round
+      ) do
+    if element >= map_size(prompts) do
+      with [r | rs] <- reviewers do
+        Map.put(round, :review_state, {r, 0})
+        |> Map.put(:reviewers_left, rs)
+      else
+        _ -> :finished
+      end
+    else
+      Map.put(round, :review_state, {player, element + 1})
     end
   end
 end
