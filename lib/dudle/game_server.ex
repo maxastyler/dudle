fa39314381bf6702614587e0097a28fcdeedf06c
Max Tyler <maxastyler@gmail.com>
2021-04-27 18:46:33 +0100

    Add a timeout

--- lib/dudle/game_server.ex
+++ lib/dudle/game_server.ex
@@ -35,6 +35,10 @@ defmodule Dudle.GameServer do
   import Access, only: [key: 1, key: 2, at: 1]
 
   @prompts ["Prompt 1", "Stinky prompt", "An incredibly cool picture", "Very very cool"]
+  @server_timeout 1 * 1000
+
+  defp wrap_timeout(xs) when is_list(xs), do: [{:timeout, @server_timeout, :any} | xs]
+  defp wrap_timeout(term), do: [term, {:timeout, @server_timeout, :any}]
 
   defp notify_of_change(%{room: room} = _data) do
     DudleWeb.Endpoint.broadcast("game:#{room}", "data_update", :data_update)
@@ -73,7 +77,7 @@ defmodule Dudle.GameServer do
   @impl true
   def init(%{room: room} = data) do
     DudleWeb.Endpoint.subscribe("presence:#{room}")
-    {:ok, :lobby, data}
+    {:ok, :lobby, data, [{:timeout, @server_timeout, :any}]}
   end
 
   defp via(name), do: {:via, Registry, {Dudle.GameRegistry, name}}
@@ -96,16 +100,16 @@ defmodule Dudle.GameServer do
 
     case map_size(players) do
       0 ->
-        {:keep_state_and_data, {:reply, from, {:error, "Can't start game with no players"}}}
+        {:keep_state_and_data, {:reply, from, {:error, "Can't start game with no players"}} |> wrap_timeout()}
 
       _ ->
         with {:ok, game} <- Game.new_game(players, @prompts) do
           new_data = Map.put(data, :game, game)
 
           {:next_state, {:playing, :submitting}, new_data,
-           [{:reply, from, :ok}, {:next_event, :internal, :notify}]}
+           [{:reply, from, :ok}, {:next_event, :internal, :notify}] |> wrap_timeout()}
         else
-          {:error, error} -> {:keep_state_and_data, {:reply, from, {:error, error}}}
+          {:error, error} -> {:keep_state_and_data, {:reply, from, {:error, error}} |> wrap_timeout()}
         end
     end
   end
@@ -124,7 +128,7 @@ defmodule Dudle.GameServer do
            |> Map.keys()
            |> Enum.map(&{&1, %{online: true}})
            |> Map.new()
-     }, {:next_event, :internal, :notify_players}}
+     }, {:next_event, :internal, :notify_players} |> wrap_timeout()}
   end
 
   def handle_event(
@@ -146,15 +150,15 @@ defmodule Dudle.GameServer do
      %{
        data
        | players: new_player_state
-     }, {:next_event, :internal, :notify_players}}
+     }, {:next_event, :internal, :notify_players} |> wrap_timeout()}
   end
 
   def handle_event({:call, from}, {:get_state, _player}, :lobby, _data) do
-    {:keep_state_and_data, {:reply, from, {:lobby, :lobby}}}
+    {:keep_state_and_data, {:reply, from, {:lobby, :lobby}} |> wrap_timeout()}
   end
 
   def handle_event({:call, from}, {:get_state, player}, {:playing, :submitting} = state, data) do
-    {:keep_state_and_data, {:reply, from, {state, Game.get_prompt(data.game, player)}}}
+    {:keep_state_and_data, {:reply, from, {state, Game.get_prompt(data.game, player)}} |> wrap_timeout()}
   end
 
   # return to the player a tuple {state, review_state, current_prompt}
@@ -166,7 +170,7 @@ defmodule Dudle.GameServer do
       ) do
     {:keep_state_and_data,
      {:reply, from,
-      {state, data.game.round.review_state, Round.get_review_prompt(data.game.round)}}}
+      {state, data.game.round.review_state, Round.get_review_prompt(data.game.round)}} |> wrap_timeout()}
   end
 
   ##### {:playing, :reviewing} events
@@ -189,9 +193,9 @@ defmodule Dudle.GameServer do
     if map_size(new_data.turn_submissions) >= length(new_data.game.players) do
       # all the submissions are in
       {:keep_state, new_data,
-       [{:reply, from, :ok}, {:next_event, :internal, :insert_submissions}]}
+       [{:reply, from, :ok}, {:next_event, :internal, :insert_submissions}] |> wrap_timeout()}
     else
-      {:keep_state, new_data, [{:reply, from, :ok}, {:next_event, :internal, :notify_players}]}
+      {:keep_state, new_data, [{:reply, from, :ok}, {:next_event, :internal, :notify_players}] |> wrap_timeout()}
     end
   end
 
@@ -210,26 +214,26 @@ defmodule Dudle.GameServer do
          new_data,
          :game,
          Map.update!(new_game, :round, &Round.convert_to_reviewing_state(&1, new_game.players))
-       ), [{:next_event, :internal, :notify_review}, {:next_event, :internal, :notify_players}]}
+       ), [{:next_event, :internal, :notify_review}, {:next_event, :internal, :notify_players}] |> wrap_timeout()}
     else
       {:keep_state, Map.put(new_data, :game, new_game),
-       [{:next_event, :internal, :notify}, {:next_event, :internal, :notify_players}]}
+       [{:next_event, :internal, :notify}, {:next_event, :internal, :notify_players}] |> wrap_timeout()}
     end
   end
 
   def handle_event(:internal, :notify, _state, data) do
     notify_of_change(data)
-    {:keep_state_and_data, []}
+    {:keep_state_and_data, [] |> wrap_timeout()}
   end
 
   def handle_event(:internal, :notify_review, _state, data) do
     notify_review_change(data)
-    {:keep_state_and_data, []}
+    {:keep_state_and_data, [] |> wrap_timeout()}
   end
 
   def handle_event(:internal, :notify_players, _state, data) do
     notify_player_change(data)
-    {:keep_state_and_data, []}
+    {:keep_state_and_data, [] |> wrap_timeout()}
   end
 
   def handle_event({:call, from}, :next_review_state, {:playing, :reviewing}, data) do
@@ -240,17 +244,17 @@ defmodule Dudle.GameServer do
                update_in(data, [:game, :previous_rounds], &[data.game.round | &1])
                |> put_in([:game, :round], new_round) do
           {:next_state, {:playing, :submitting}, new_data,
-           [{:reply, from, :ok}, {:next_event, :internal, :notify}]}
+           [{:reply, from, :ok}, {:next_event, :internal, :notify}] |> wrap_timeout()}
         end
 
       new_round ->
         {:keep_state, put_in(data, [:game, :round], new_round),
-         [{:reply, from, :ok}, {:next_event, :internal, :notify_review}]}
+         [{:reply, from, :ok}, {:next_event, :internal, :notify_review}] |> wrap_timeout()}
     end
   end
 
   def handle_event({:call, from}, _, _, _) do
-    {:keep_state_and_data, [{:reply, from, {:error, "event unhandled"}}]}
+    {:keep_state_and_data, [{:reply, from, {:error, "event unhandled"}}] |> wrap_timeout()}
   end
 
   ##### :end events
